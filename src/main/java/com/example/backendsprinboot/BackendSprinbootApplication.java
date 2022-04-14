144c92f368d09151e66a0f09b975edff756491fa
Evgeniy <46program@gmail.com>
2022-04-14 15:06:55 +0300

    Initial commit

---
+++ src/main/java/com/example/backendsprinboot/BackendSprinbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendsprinboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSprinbootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BackendSprinbootApplication.class, args);
+  }
+
+}
