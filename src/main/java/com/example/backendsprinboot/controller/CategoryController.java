6e347e003cdb8a68725cbefe422157ea9704e4ee
Evgeniy <46program@gmail.com>
2022-06-13 20:33:15 +0300

    Connecting frontend

--- src/main/java/com/example/backendsprinboot/controller/CategoryController.java
+++ src/main/java/com/example/backendsprinboot/controller/CategoryController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/category")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class CategoryController {
 
   HttpHeaders header = new HttpHeaders();
--- src/main/java/com/example/backendsprinboot/controller/TaskController.java
+++ src/main/java/com/example/backendsprinboot/controller/TaskController.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/task")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class TaskController {
 
   private TaskService taskService;
